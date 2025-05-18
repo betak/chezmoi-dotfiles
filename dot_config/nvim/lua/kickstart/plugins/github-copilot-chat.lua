0e827f0f40304e11a2a67560455af6b516ee04f1
Travis Betak <betak@fastmail.com>
2025-05-18 15:31:00 -0500

    Add neovim configuration files

---
+++ dot_config/nvim/lua/kickstart/plugins/github-copilot-chat.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or zbirenbaum
+      { 'nvim-lua/plenary.nvim', branch = 'master' }, -- curl, log and async functions
+    },
+    opts = {},
+  },
+}
