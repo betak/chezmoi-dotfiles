c36d83af0e905c28fadb054667ebde09dc1ff60f
Travis Betak <betak@fastmail.com>
2025-05-19 12:32:17 -0500

    Add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.automatically_reload_config = true
+
+config.color_scheme = "Black Metal (Immortal) (base16)"
+-- config.color_scheme = "Black Metal (Gorgoroth) (base16)"
+
+config.font = wezterm.font("JetBrains Mono")
+
+return config
